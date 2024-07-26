d4fcd033b8010fb2b8ca93af4cb2db7fec48063d
Marco <marco.cremaschi@unimib.it>
2024-07-26 13:10:03 +0200

    Inserito esempio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,10 @@ export default function Home(): JSX.Element {
       title={`Hello from ${siteConfig.title}`}
       description="MantisTable UI was designed to let you tame your Semantic Table Interpretation approach.">
       <main>
-        <div>MAIN</div>
+        <div>
+          QUI PUOI INSERIRE HTML E TAILWIND
+          <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">Badge</span>
+        </div>
       </main>
     </Layout>
   );
