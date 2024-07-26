f8041b10e32aca533972460936e5858d7df5d645
Marco <marco.cremaschi@unimib.it>
2024-07-26 13:04:29 +0200

    clean home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,29 +7,14 @@ import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-      </div>
-    </header>
-  );
-}
-
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="MantisTable UI was designed to let you tame your Semantic Table Interpretation approach.">
-      <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        <div>MAIN</div>
       </main>
     </Layout>
   );
