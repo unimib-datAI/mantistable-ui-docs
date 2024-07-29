ed91c3d5a3390a6f1194c2831b66e2a7ef97a076
giulia <g.avis@campus.unimib.it>
2024-07-29 17:52:17 +0200

    Copy and fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,34 +7,27 @@ import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
 
-import { Check, ArrowRight } from 'lucide-react';
+import { Check, ArrowRight, SquarePen, SquareChartGantt, Blocks } from 'lucide-react'; 
 
 
 const features = [
   {
-    name: 'Feature 1',
+    name: 'Friendly UI system for interacting and managing STI approaches',
     description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-    icon: Check,
+      'We know designing a user-friendly, robust UI can be a challenge and can demand both technical prowess and design skills, that is why we developed MantisTable UI',
+    icon: SquareChartGantt,
   },
   {
-    name: 'Feature 2',
-    description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
-    icon: Check,
+    name: 'Visualisation of annotations',
+    description: 'With MantisTable UI, you can easily visualise table annotations, providing a clear and intuitive overview of your data',
+    icon: SquarePen,
   },
   {
-    name: 'Feature 3',
-    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
-    icon: Check,
-  },
-
-  {
-    name: 'Feature 4',
-    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
-    icon: Check,
+    name: 'Powerful and flexible plugin system',
+    description: 'This incredible feature enables users to easily extend and customise its functionality, offering a seamless experience and allowing for effortless expansion and personalisation',
+    icon: Blocks,
   },
 ]
-
 
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
@@ -43,7 +36,7 @@ export default function Home(): JSX.Element {
       title={`Hello from ${siteConfig.title}`}
       description="MantisTable UI was designed to let you tame your Semantic Table Interpretation approach.">
       <main>
-      <section className="bg-lgBackground dark:bg-dgBackground">
+      <section>
         <div className="place-content-center mx-4 lg:gap-12 xl:gap-12 lg:pt-10 lg:grid-cols-12 pb-12">
         <h1 className="md:text-center pt-12 mb-4 leading-none text-gray-900 sm:text-6xl md:text-7xl lg:text-8xl dark:text-white font-bold tracking-widest">MantisTable UI</h1>
         <div className="justify-center flex flex-col md:flex-row max-w-6xl gap-20 pb-2 mx-auto">
@@ -55,8 +48,8 @@ export default function Home(): JSX.Element {
         <p
         className=" text-dgBackground dark:text-white sm:text-xl lg:mb-8 md:text-2xl lg:text-3xl"
       > Effortless data interaction and simple table navigation with a Graphic User Interface for Semantic Table Interpretion </p>
-                <button className="text-center inline-flex items-center border-none dark:bg-primaryGreen dark:text-lightBackground text-dgBackground font-sm rounded-lg text-sm px-5 py-2.5 me-2 mb-2 bg-secondaryGreen font-semibold"> 
-                Learn more <ArrowRight className="ml-2 h-5 w-5"/> </button>
+                <a href="mantistable-ui-docs/docs/introduction" className="text-center inline-flex items-center border-none dark:bg-primaryGreen dark:text-lightBackground text-dgBackground font-sm rounded-lg text-sm px-5 py-2.5 me-2 mb-2 bg-secondaryGreen font-semibold"> 
+                Learn more <ArrowRight className="ml-2 h-5 w-5"/> </a>
         </div>
 
         </div>
@@ -69,10 +62,9 @@ export default function Home(): JSX.Element {
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="flex flex-col justify-center lg:pr-8 lg:pt-4">
             <div className="lg:max-w-lg">
-              <h2 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl">An intuitive UI</h2>
+              <h2 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl">An intuitive UI for Semantic Table Interpretation</h2>
               <p className="mt-4 text-lg leading-6 text-gray-600 dark:text-gray-300">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
-                iste dolor cupiditate blanditiis ratione.
+              MantisTable UI is the ultimate web interface for managing Semantic Table Interpretation (STI) approaches, providing an intuitive, ready-to-use platform tailored anyone who is interested in STI world.
               </p>
               <div className="mt-6 max-w-xl space-y-4 text-base leading-5 text-gray-600 lg:max-w-none">
                 {features.map((feature) => (
