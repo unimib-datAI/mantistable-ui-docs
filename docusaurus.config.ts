fa4e411ba224299e88fe461364f59bca187eb93c
Marco <marco.cremaschi@unimib.it>
2024-08-01 15:59:38 +0200

    fix url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://unimibinside.github.io',
+  url: 'https://unimib-datai.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/mantistable-ui-docs/',
